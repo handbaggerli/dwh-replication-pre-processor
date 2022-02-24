b44388254e86bdec2c37978d52c8dafb3458c9e0
Markus <markus.merkle277@gmail.com>
2022-02-24 10:06:40 +0100

    Start Initial Setup

---
+++ src/main/java/ch/centrisag/dwh/dwhreplicationpreprocessor/DwhReplicationPreProcessorApplication.java
@@ -0,0 +1,13 @@
+package ch.centrisag.dwh.dwhreplicationpreprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhReplicationPreProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwhReplicationPreProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/centrisag/dwh/dwhreplicationpreprocessor/DwhReplicationPreProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.centrisag.dwh.dwhreplicationpreprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwhReplicationPreProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
